b763b242923ab48e67b91af862cac76c2d3c4ef9
Jarne2001 <jarnemelis01@gmail.com>
2025-11-26 18:48:21 +0100

    Update Feature_Engineering.py

--- Feature_Engineering.py
+++ Feature_Engineering.py
@@ -30,7 +30,6 @@ df.loc[mask_left, 'ball_land_x'] = 120.0 - df.loc[mask_left, 'ball_land_x']
 # mirror horizontally longitudinal velocity
 df.loc[mask_left, 'vx'] = -df.loc[mask_left, 'vx']
 
-# Recompute from vx,vy
 new_rad = np.arctan2(df['vy'], df['vx'])
 df['dir'] = (np.rad2deg(new_rad) % 360)
 
@@ -40,6 +39,19 @@ defenders_radius=5.0
 linear_projection=1.0
 eps=1e-6
 last_n = 3
+
+height = df['player_height'].str.split('-', expand=True)
+
+# Convert to numeric
+feet = pd.to_numeric(height[0], errors='coerce')
+inches = pd.to_numeric(height[1], errors='coerce')
+
+# Compute total height in inches
+df['player_height_inches'] = feet * 12 + inches
+
+df['player_birth_date'] = pd.to_datetime(df['player_birth_date'], errors='coerce')
+
+df['player_age'] = 2023 - df['player_birth_date'].dt.year
 
 # Angles because models dont enjoy wrapping around angles
 rad = np.deg2rad(df['dir'])
@@ -66,15 +78,19 @@ release_df['ball_angle'] = np.arctan2(release_df['ball_dy'], release_df['ball_dx
 # signed angular difference in radians, in [-pi, pi]
 release_df['dir_rad'] = np.deg2rad(release_df['dir'])
 release_df['angle_diff_ball'] = ((release_df['ball_angle'] - release_df['dir_rad'] + np.pi) % (2*np.pi)) - np.pi
-release_df['eta_to_ball'] = release_df['distance_to_ball'] / (release_df['s'].replace(0, eps))
+release_df['eta_to_ball'] = release_df['distance_to_ball'] / release_df['s'].replace(0, eps)
+release_df['eta_to_ball'] = release_df['eta_to_ball'].clip(upper=15)
 speed_norm = np.hypot(release_df['vx'], release_df['vy']).replace(0, eps)
 release_df['heading_alignment'] = (release_df['vx'] * release_df['ball_dx'] + release_df['vy'] * release_df['ball_dy']) / (speed_norm * release_df['distance_to_ball'].replace(0, eps))
 
 # Linear projection features 
 release_df['projection_x'] = release_df['x'] + release_df['vx'] * linear_projection
 release_df['projection_y'] = release_df['y'] + release_df['vy'] * linear_projection
+release_df['projection_x'] = release_df['projection_x'].clip(0, 120)
+release_df['projection_y'] = release_df['projection_y'].clip(0, 53.3)
 release_df['projection_distance_to_ball'] = np.hypot(release_df['projection_x'] - release_df['ball_land_x'], 
                                                   release_df['projection_y'] - release_df['ball_land_y'])
+release_df['endzone_distance'] = 120.0 - release_df['x']
 # Deltas from previous frames
 df_sorted = df.sort_values(['game_id','play_id','nfl_id','frame_id'])
 def last_deltas(g):
@@ -109,8 +125,6 @@ else:
 release_df = release_df.merge(defence_min, on=['game_id','play_id'], how='left')
 release_df = release_df.merge(defence_count, on=['game_id','play_id'], how='left')
 release_df[f'n_def_within_{int(defenders_radius)}'] = release_df[f'n_def_within_{int(defenders_radius)}'].fillna(0).astype(int)
-
-
 
 # Nearest defender
 min_defender_to_receiver_dist = []
@@ -158,10 +172,36 @@ release_df['player_avg_acceleration'] = avg_acceleration
 release_df['velocity_x'] = release_df['s'] * np.cos(release_df['dir_rad'])
 release_df['velocity_y'] = release_df['s'] * np.sin(release_df['dir_rad'])
 
-release_df['momentum_x'] = release_df['velocity_x'] * release_df['player_weight']
-release_df['momentum_y'] = release_df['velocity_y'] * release_df['player_weight']
+release_df['momentum_x'] = (release_df['velocity_x'] * release_df['player_weight'])/1000
+release_df['momentum_y'] = (release_df['velocity_y'] * release_df['player_weight'])/1000
 
 release_df['left_distance'] = release_df['y']
 release_df['right_distance'] = 53.3 - release_df['y']
 
-release_df.to_csv("release_df.csv", index=False)
+# Feature evaluation
+features = [
+    'game_id','play_id','nfl_id','frame_id','x','y','s','a','dir','o','vx','vy',
+    'player_position','player_role','player_side',
+    'dir_sin','dir_cos','o_sin','o_cos','dir_rad',
+    'ball_dx','ball_dy','ball_land_x','ball_land_y','distance_to_ball','ball_angle','angle_diff_ball','eta_to_ball',
+    'heading_alignment','projection_x','projection_y','projection_distance_to_ball', 'endzone_distance',
+    'dx_last','dy_last','ds_last','ddir_last',
+    'min_defender_dist_to_landing', f'n_def_within_{int(defenders_radius)}',
+    'min_defender_to_receiver_dist','min_defender_speed','relative_speed_nearest_defender',
+    'player_height_inches','player_weight','player_birth_date','player_age','player_avg_speed','player_avg_acceleration',
+    'velocity_x','velocity_y','momentum_x','momentum_y']
+
+# check NaN % number
+nan_percent = (release_df[expected].isna().mean() * 100).sort_values(ascending=False)
+print(nan_percent)
+
+# Summary statistics
+print("\n--- Summary Stats ---")
+print(release_df[expected[:7]].describe())
+print(release_df[expected[7:14]].describe())
+print(release_df[expected[14:21]].describe())
+print(release_df[expected[21:27]].describe())
+print(release_df[expected[27:]].describe())
+release_df.dtypes
+
+release_df.to_csv("features_data.csv", index=False)
